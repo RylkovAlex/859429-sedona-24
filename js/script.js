c412a947e0a26e5c00561db487b17913b5cebf1e
RylkovAlex <32511612+RylkovAlex@users.noreply.github.com>
2019-02-24 14:51:34 +0300

    экспериментирую с z-index и анимацией формы

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 var link = document.querySelector(".booking-search-button");
 var popup = document.querySelector(".booking-popup");
 var focus_input = popup.querySelector("[name=arrival-date]");
+var click = 0;
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-    popup.classList.toggle("popup-hide");
-    popup.classList.toggle("popup-appear");
-    focus_input.focus();
+  click++;
+  if (click % 2 === 0) {
+  popup.classList.remove("popup-appear");
+  popup.classList.add("popup-hide");
+  } else {
+  popup.classList.remove("popup-hide");
+  popup.classList.add("popup-appear");
+  focus_input.focus();
+  }
 });
