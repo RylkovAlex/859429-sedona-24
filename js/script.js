b9bb808c1c6d18afa31c962e06db6897943e5b52
RylkovAlex <32511612+RylkovAlex@users.noreply.github.com>
2019-02-24 17:30:26 +0300

    вариант с анимацией и 2мя классами

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 var link = document.querySelector(".booking-search-button");
 var popup = document.querySelector(".booking-popup");
 var focus_input = popup.querySelector("[name=arrival-date]");
-var click = 0;
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  click++;
-  if (click % 2 === 0) {
-  popup.classList.remove("popup-appear");
-  popup.classList.add("popup-hide");
-  } else {
-  popup.classList.remove("popup-hide");
-  popup.classList.add("popup-appear");
+  popup.classList.toggle("popup-appear");
   focus_input.focus();
+  if (popup.classList.contains("popup-appear")) {
+    popup.classList.remove("popup-hide");
+  } else {
+    popup.classList.add("popup-hide");
   }
 });
